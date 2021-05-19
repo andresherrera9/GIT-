c69f640c82639422edbd48ab34d1c6572ebd7826
FELIPE HERRERA <andres.herrera92@hotmail.com>
2021-05-19 08:47:08 -0500

    Update settings.py

--- MiSitioFinal/MiSitioFinal/settings.py
+++ MiSitioFinal/MiSitioFinal/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'pj3_#txk7z7)a(wnx3)dzc$6r1^l%5m&_rok#&rds6^xh(un!9'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
